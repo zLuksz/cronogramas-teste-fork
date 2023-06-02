35add2c0fffd3de64170f347f18804d23b3186f7
Lucas Cavalcante <Luquinhalcz@gmail.com>
2023-06-02 11:38:40 -0300

    controllersunidade. Lucas Cavalcante

--- src/controllers/controllerCurso.ts
+++ src/controllers/controllerCurso.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import CursoService from "../services/serviceCurso"
+import { CursoService } from "../services/serviceCurso"
 
 const service = new CursoService()
 
