f5a0f9a2a3cf016bc1389356f463bcf684e2f921
Danielle Almeida <daniellealmeidagms@gmail.com>
2023-06-02 11:18:49 -0300

    atualiza serviceCurso e cria controllerCurso

--- src/services/serviceCurso.ts
+++ src/services/serviceCurso.ts
@@ -28,7 +28,7 @@ type findOneCursoRequest = {
 
 // 3) Funções CRUD
 
-export class CursoService {
+export default class CursoService {
   async create({
     descricao_curso,
     carga_horaria_curso,
@@ -90,12 +90,12 @@ export class CursoService {
     return curso
   }
 
-  async delete({ id_curso }: findOneCursoRequest): Promise<Curso | Error> {
+  async delete({ id_curso }: findOneCursoRequest): Promise<String | Error> {
     const curso = await cursor.findOne({ where: { id_curso } })
     if (!curso) {
       return new Error("Curso não encontrado!")
     }
     await cursor.delete(curso.id_curso)
-    return curso
+    return "Curso excluído com sucesso!"
   }
 }
