f00d1c8ea656e80c2825a1cfb4f32cb0cb5df909
Lucas Cavalcante <Luquinhalcz@gmail.com>
2023-06-07 11:14:35 -0300

    rotas

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { AppDataSource } from "./databases/connections/data-source"
+import rotas from "./routes/routes"
+
 AppDataSource.initialize()
 
 const express = require("express")
 const app = express()
 app.use(express.json())
-
-app.get("/", (request, response) => {
-  return response.json("E ai, suave?")
-})
+app.use(rotas)
 
 app.listen(3333, () => console.log("O server esta ON na porta 3333."))
