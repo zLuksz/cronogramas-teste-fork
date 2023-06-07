1dc939661996ae8eb5a279f3a484e3e026b09982
Jaquellyne Teles <jackstelles@gmail.com>
2023-06-07 11:28:42 -0300

    corrige rotas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,50 +1,33 @@
-import { Router } from "express";
+import { Router } from "express"
 import CursoController from "../controllers/controllerCurso"
-
-const rotas = Router()
-
-rotas.get("/", (request, response) => {
-    return response.json("Home Page")
-})
-
-rotas.post("/cursos", new CursoController().create)
-rotas.post("/cursos", new CursoController().readAll)
-rotas.post("/cursos/:id", new CursoController().readOne)
-rotas.post("/cursoss/:id", new CursoController().update)
-rotas.post("/cursos/id", new CursoController().delete)
-
-export default rotas
-
-import { Router } from "express";
 import TurmaController from "../controllers/controllerTurma"
-
-const rotas = Router()
-
-rotas.get("/", (request, response) => {
-    return response.json("Home Page")
-})
-
-rotas.post("/turmas", new TurmaController().create)
-rotas.post("/turmas", new TurmaController().readAll)
-rotas.post("/turmas/:id", new TurmaController().readOne)
-rotas.post("/turmas/:id", new TurmaController().update)
-rotas.post("/turmas/id", new TurmaController().delete)
-
-export default rotas
-
-import { Router } from "express";
 import UnidadeController from "../controllers/controllerUnidade"
 
 const rotas = Router()
 
 rotas.get("/", (request, response) => {
-    return response.json("Home Page")
+    return response.json("home page")
 })
 
-rotas.post("/unidades", new UnidadeController().create)
-rotas.post("/unidades", new UnidadeController().readAll)
-rotas.post("/unidades/:id", new UnidadeController().readOne)
-rotas.post("/unidades/:id", new UnidadeController().update)
-rotas.post("/unidades/id", new UnidadeaController().delete)
+//Curso
+rotas.post("/cursos", new CursoController().create)
+rotas.get("/cursos", new CursoController().readAll)
+rotas.get("/cursos/:id", new CursoController().readOne)
+rotas.put("/cursos/:id", new CursoController().update)
+rotas.delete("/cursos/:id", new CursoController().delete)
+
+//Turma
+rotas.post("/turmas", new TurmaController().create)
+rotas.get("/turmas", new TurmaController().readAll)
+rotas.get("/turmas/:id", new TurmaController().readOne)
+rotas.put("/turmas/:id", new TurmaController().update)
+rotas.delete("/turmas/:id", new TurmaController().delete)
+
+//Unidade
+rotas.post("/Unidades", new UnidadeController().create)
+rotas.get("/Unidades", new UnidadeController().readAll)
+rotas.get("/Unidades/:id", new UnidadeController().readOne)
+rotas.put("/Unidades/:id", new UnidadeController().update)
+rotas.delete("/Unidades/:id", new UnidadeController().delete)
 
 export default rotas
