2d981e5ee012901e330babb470fb922899a5fd8d
Danielle Almeida <daniellealmeidagms@gmail.com>
2023-06-12 11:13:06 -0300

    separa datasources Dev e Prod

---
+++ src/databases/connections/datasourceProd.ts
@@ -0,0 +1,17 @@
+import { DataSource } from "typeorm"
+import Curso from "../models/curso"
+import Unidade from "../models/unidade"
+import Turma from "../models/turma"
+import Recesso from "../models/recesso"
+
+export const ProdDataSource = new DataSource({
+  type: "postgres",
+  host: "dpg-ci2iosak728i8t9n7i8g-a.oregon-postgres.render.com",
+  port: 5432,
+  username: "dev",
+  password: "s9qzDErug5tmpW7lqzM2WHFKJgprRYOS",
+  database: "cronogramas",
+  synchronize: true,
+  logging: true,
+  entities: [Curso, Unidade, Turma, Recesso],
+})
